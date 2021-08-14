a329913aa8b684edbf6efd1bd881015c453b8859
artginzburg <art.ginzburg@ya.ru>
2021-08-14 12:55:19 +0300

    start making header

---
+++ src/components/App.jsx
@@ -0,0 +1,37 @@
+function App() {
+  return (
+    <>
+      <header className="header">
+        <a target="_self" href="https://wannaseedeadbody.github.io" className="logo header__logo">
+          OUTSIDEINSIDEOUT
+        </a>
+        <nav>
+          <ul class="header__links">
+            <li class="header__link-container">
+              <a href="" class="header__link">
+                About me
+              </a>
+            </li>
+            <li class="header__link-container">
+              <a href="" class="header__link">
+                Portfolio
+              </a>
+            </li>
+            <li class="header__link-container">
+              <a href="" class="header__link">
+                Contacts
+              </a>
+            </li>
+            <li class="header__link-container">
+              <a href="https://calendar.yandex.ru" class="header__link">
+                Start project
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
